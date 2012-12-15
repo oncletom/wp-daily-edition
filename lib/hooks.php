ee8fda3e1b2b38351502dace1e06ce51f3769d6d
oncletom <thomas@oncle-tom.net>
2012-12-15 19:30:11 +0100

    Filtering archives

--- lib/hooks.php
+++ lib/hooks.php
@@ -10,10 +10,25 @@ class Hooks{
         }
 
         add_action('parse_query', array('\DailyEdition\Hooks', 'filterHomeQuery'));
+        add_action('parse_query', array('\DailyEdition\Hooks', 'filterArchiveQuery'));
+        add_action('archive_template', array('\DailyEdition\Hooks', 'filterArchiveTemplate'));
     }
 
-    function filterHomeQuery(\WP_Query &$query){
-        if ($query->is_main_query() && is_home()){
+    /**
+     * Adds the proper parameters to make frontpage and daily pagination the right content
+     *
+     * @param \WP_Query $query
+     */
+    public static function filterHomeQuery(\WP_Query &$query){
+        /*
+         * Nothing to do
+         */
+        if (!$query->is_main_query()){
+            return $query;
+        }
+
+        // Front Page
+        if (is_home()){
             $date = get_latest_edition_date();
 
             $query->set('year', $date['year']);
@@ -21,11 +36,51 @@ class Hooks{
             $query->set('day', $date['day']);
         }
 
-        if ($query->is_main_query() && (is_home() || is_archive())){
+        // Daily Pagination
+        if (is_home() || (is_archive() && is_day())){
             $query->set('nopaging', true);
             $query->set('meta_key', 'daily-edition-order');
             $query->set('orderby', 'meta_value_num');
             $query->set('order', 'ASC');
         }
+    }
+
+    public static function filterArchiveQuery(\WP_Query &$query){
+        /*
+         * Nothing to do
+         */
+        if (!$query->is_main_query()){
+            return $query;
+        }
+
+        /*
+         * Yearly and Monthly
+         */
+        if (is_archive() && (is_year() || is_month())){
+            $query->set('cat', get_cat_ID('Edito'));
+        }
+
+        /*
+         * Categories and tags
+         */
+        if (is_category() || is_tag()){
+
+        }
+    }
+
+    /**
+     * For daily pagination, we still rely on the index
+     * So far, index and daily pagination has the same appearance
+     *
+     * @param string $template_file
+     * @return string
+     */
+    public static function filterArchiveTemplate($template_file){
+
+        if (is_day()){
+            $template_file = locate_template('index.php');
+        }
+
+        return $template_file;
     }
 }
